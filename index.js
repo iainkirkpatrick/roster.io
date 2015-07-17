884dc45f64f57a5215921b318ed3bb3d4e958a05
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-07-17 15:01:41 +1200

    subtle changes to index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var React = require('react');
 var Table = require('./table')
 
-React.render(<Table width={600} height={300} rows={9} columns={5} />, document.body);
+React.render(<Table width={800} height={600} rows={9} columns={5} />, document.body);
